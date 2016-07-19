cf084263d2b5cdabe936968e949698bf0330e1ac
Nicole Peoples <nicolecpeoples@gmail.com>
2016-07-19 10:53:59 -0700

    full friends

---
+++ full_mean/assignments/004_all_friends/client/assets/controllers/editController.js
@@ -0,0 +1,19 @@
+app.controller('editController', ['$scope', 'friendsFactory', '$location', '$routeParams', 'uibDateParser', function($scope, friendsFactory, $location, $routeParams, uibDateParser){
+
+	var self = this; 
+	self.friend = {};
+	self.friend_id = $routeParams;
+	$scope.format = 'MM/dd/yyyy';
+  	$scope.date = new Date();
+
+	friendsFactory.getFriend(self.friend_id, function(returned_data){
+		self.friend = returned_data.data[0];
+	})
+
+	self.edit = function() {
+		console.log(self.friend);
+		friendsFactory.update(self.friend);
+		$location.path('/');
+	}
+
+}]);
